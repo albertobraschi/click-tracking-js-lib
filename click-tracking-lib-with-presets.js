ce0c9425398bfb80fc8d5dece82ca09405bbb3f2
Erik Vold <erikvvold@gmail.com>
2009-12-11 16:37:08 -0800

    - bug fix: missing '}' - minor whitespace updates

--- click-tracking-lib-with-presets.js
+++ click-tracking-lib-with-presets.js
@@ -1,40 +1,39 @@
 (function(){
 	function _clickTrackingLib(){
-		var matchAry = [],
-			RegExpCache = [ /^(http|ftp|chrome|file|view-source)/i ];
+		var matchAry = [], RegExpCache = [/^(http|ftp|chrome|file|view-source)/i];
 
-		if (document.body.addEventListener){
+		if ( document.body.addEventListener ) {
 			// w3c
 			var addClickEvent = function( ele, evt ){
 				ele.addEventListener( "click", evt, false );
 			};
 		}
-		else if ( document.body.attachEvent ){
+		else if ( document.body.attachEvent ) {
 			var addClickEvent = function( ele, evt ){
 				ele.attachEvent( "onclick", evt );
 			};
 		}
 
-		if (document.body.removeEventListener){
+		if ( document.body.removeEventListener ) {
 			// w3c
 			var removeClickEvent = function( ele, evt ){
 				ele.removeEventListener( "click", evt, false );
 			};
 		}
-		else if ( document.body.detachEvent ){
+		else if ( document.body.detachEvent ) {
 			var removeClickEvent = function( ele, evt ){
 				ele.detachEvent( "onclick", evt );
 			};
 		}
 
 		var delayClickEventHandler = function( e, delay ){
-			if( !e ) var e = window.event;
+			if ( !e ) var e = window.event;
 
 			var targ;
-			if( e.target ) targ = e.target;
-			else if( e.srcElement ) targ = e.srcElement;
+			if ( e.target ) targ = e.target;
+			else if ( e.srcElement ) targ = e.srcElement;
 			// Safari..
-			if( targ.nodeType == 3 ) targ = targ.parentNode;
+			if ( targ.nodeType == 3 ) targ = targ.parentNode;
 
 			setTimeout( 'window.location = "' + targ.href + '"', delay );
 
@@ -50,47 +49,46 @@
 		};
 
 		this.attachTrackingFunctions = function( links, delay ){
-			if( matchAry.length == 0 ) return;
+			if ( matchAry.length == 0 ) return;
 
-			var links = ( links.href ) ? [ links ] : ( links.length ) ? links : ( !links ) ? document.links : [];
+			var links = ( links && links.href ) ? [links] : ( links && links.length ) ? links : ( !links ) ? document.links : [];
 
-			if( !links.length ) return;
+			if (!links.length) return;
 
 			var link,
-				z,
-				clickFunc,
 				matchAryLen = matchAry.length,
 				delay = (delay != undefined) ? ((delay) ? delay : false) : 99;
 
-			if( delay ){
-				var delayFunc = function( e ){
-					delayClickEventHandler( e, delay );
+			if (delay) {
+				var delayFunc = function(e){
+					delayClickEventHandler(e, delay);
 				};
 			}
 
 			// loop through the links
-			for( var i = links.length - 1; i >= 0; i-- ){
+			for (var i = links.length - 1; i >= 0; i--) {
 				link = links[i];
 
 				// get a reference to the current link which can be used later
 				(function( link ){
 					// loop through the the remaining match functions in the array of provided match functions
-					for(var j = 0; j < matchAryLen; j++ ){
+					for ( var j = 0; j < matchAryLen; j++ ) {
 						// check link with provided match function
-						if( matchAry[j].match( link ) ){
+						if ( matchAry[j].match( link ) ) {
 							// Add provided click event wrapped in another lambda providing a reference to the link element
-							(function(j){
+							(function( j ){
 								// Attach the wrapper lambda to the link's click event
 								addClickEvent( link, function( e ){
 									matchAry[j].trackingFunc( e, link );
-								} );
-							})(j);
+								});
+							})( j );
 						}
 					}
 				})( link );
 
 				// check if a delay function should be applied
-				if( delay === false ||
+				if (
+					delay === false ||
 					( link.target && ( link.target == "_blank" || link.target == "_new" ) ) ||
 					!link.protocol.match( RegExpCache[0] )
 				) continue;
@@ -104,6 +102,7 @@
 
 			return;
 		};
+	}
 
 	// create the main clickTrackingLib instance
 	clickTrackingLib = new _clickTrackingLib();
